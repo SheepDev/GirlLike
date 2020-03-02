d7d815329d914413415cf82bf4a2e2eb85591286
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-03-02 02:47:15 -0300

    Create player Input and movement

---
+++ Assets/GirlLike/Script/Runtime/Player/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using Orb.GirlLike.Controllers;
+using UnityEngine;
+
+namespace Orb.GirlLike.Players
+{
+  public class PlayerInput : MonoBehaviour
+  {
+    public Controller controller;
+
+    private void Awake()
+    {
+      var movement = GetComponent<PlayerMovement>();
+
+      controller.jump.onUpdate += movement.Jump;
+      controller.horizontal.onUpdate += movement.SetHorizontal;
+    }
+  }
+}
---
+++ Assets/GirlLike/Script/Runtime/Player/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using Orb.GirlLike.Controllers;
+using UnityEngine;
+
+namespace Orb.GirlLike.Players
+{
+  public class PlayerMovement : MonoBehaviour
+  {
+    public float speed;
+    public float jumpForce;
+
+
+    [Header("Ground Settings")]
+    public Vector2 boxSize;
+    public float maxDistance;
+    public LayerMask layerGround;
+
+    private float horizontalAxis;
+    private Transform cacheTransform;
+
+    public Rigidbody2D _rigidbody { get; private set; }
+
+    private void Awake()
+    {
+      _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+      SetXVelocity(horizontalAxis * speed);
+    }
+
+    public void Jump(ActionState state)
+    {
+      var isJump = Input.GetKeyDown(KeyCode.Space) && IsGround(out var hit);
+      if (!isJump) return;
+
+      var velocity = _rigidbody.velocity;
+      velocity.y = 0;
+      _rigidbody.velocity = velocity;
+      _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void SetHorizontal(float value)
+    {
+      horizontalAxis = value;
+    }
+
+    private void SetXVelocity(float value)
+    {
+      var velocity = _rigidbody.velocity;
+      velocity.x = value;
+      _rigidbody.velocity = velocity;
+    }
+
+    public bool IsGround(out RaycastHit2D hit)
+    {
+      var transform = GetTransform();
+      var origin = transform.position;
+      hit = Physics2D.BoxCast(origin, boxSize, 0, Vector2.down, maxDistance, layerGround);
+      return hit.collider != null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+      var transform = GetTransform();
+      var origin = transform.position;
+
+      var isGround = IsGround(out var hit);
+      Gizmos.color = Color.white;
+      Gizmos.DrawWireCube(origin, boxSize);
+
+      if (isGround)
+      {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(hit.centroid, boxSize);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(origin, hit.centroid);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hit.point, 0.1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(hit.point, hit.point + hit.normal);
+      }
+      else
+      {
+        var centroid = origin + Vector3.down * (maxDistance);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(centroid, boxSize);
+        Gizmos.DrawLine(origin, centroid);
+      }
+    }
+
+    public Transform GetTransform()
+    {
+      if (cacheTransform == null)
+      {
+        cacheTransform = transform;
+      }
+
+      return cacheTransform;
+    }
+  }
+}
