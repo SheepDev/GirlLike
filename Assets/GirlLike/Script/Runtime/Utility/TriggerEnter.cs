bfe298a43b75940d3e23ce8385c807d1de90366a
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-03-10 20:27:30 -0300

    Use tag for trigger in TriggerEnter

--- Assets/GirlLike/Script/Runtime/Utility/TriggerEnter.cs
+++ Assets/GirlLike/Script/Runtime/Utility/TriggerEnter.cs
@@ -1,3 +1,4 @@
+using Orb.GirlLike.Settings;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,12 +6,16 @@ namespace Orb.GirlLike.Utility
 {
   public class TriggerEnter : MonoBehaviour
   {
+    public Tag selectTag;
+
     [Header("Events")]
     public UnityEvent onEnter;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-      onEnter.Invoke();
+      var tagName = selectTag.ToString();
+      if (string.IsNullOrEmpty(tagName) || other.CompareTag(tagName))
+        onEnter.Invoke();
     }
   }
 }
