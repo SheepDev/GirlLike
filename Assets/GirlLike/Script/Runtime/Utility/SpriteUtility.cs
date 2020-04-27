2637b008607465b5fef4882ed7bcef3d48804561
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-04-27 13:52:47 -0300

    Add default value for Flip in spriteUtility.cs

--- Assets/GirlLike/Script/Runtime/Utility/SpriteUtility.cs
+++ Assets/GirlLike/Script/Runtime/Utility/SpriteUtility.cs
@@ -6,13 +6,14 @@ namespace Orb.GirlLike.Utility
   public class SpriteUtility : MonoBehaviour
   {
     public SpriteRenderer sprite;
+    public bool defaultFlipX;
     public Transform flipTransform;
 
     public void FlipX(bool isFlip)
     {
+      isFlip = isFlip ? !defaultFlipX : defaultFlipX;
       var scale = Vector3.one;
       if (isFlip) scale.x *= -1;
-
       sprite.flipX = isFlip;
       flipTransform.localScale = scale;
     }
