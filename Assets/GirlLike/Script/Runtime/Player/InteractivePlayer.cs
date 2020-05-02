e017d106b46a4d08922cb927699268ef5ada7e86
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-05-02 01:28:20 -0300

    Add on interactive event in InteractivePlayer.cs

--- Assets/GirlLike/Script/Runtime/Player/InteractivePlayer.cs
+++ Assets/GirlLike/Script/Runtime/Player/InteractivePlayer.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Orb.GirlLike.Players
 {
   public class InteractivePlayer : MonoBehaviour
   {
+    public UnityEvent onInteractive;
+
     public virtual void Interactive(Player player)
     {
-      Debug.Log("Player");
+      onInteractive.Invoke();
     }
   }
 }
