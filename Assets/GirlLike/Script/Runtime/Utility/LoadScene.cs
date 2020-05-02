25bda4299205a3d3749c967521bfceae462e1f6d
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-05-02 01:29:18 -0300

    Create LoadScene.cs

---
+++ Assets/GirlLike/Script/Runtime/Utility/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Orb.GirlLike.Utility
+{
+  public class LoadScene : MonoBehaviour
+  {
+    public void Load(int buildIndex)
+    {
+      SceneManager.LoadScene(buildIndex);
+    }
+  }
+}
