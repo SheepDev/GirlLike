42207a232e74745b2af7d90868cf70c569f63ec4
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-05-02 01:29:59 -0300

    Create AudioManager and Master mixer

---
+++ Assets/GirlLike/Script/Runtime/Audio/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+
+namespace Orb.GirlLike.Audio
+{
+  public class AudioManager : MonoBehaviour
+  {
+    [SerializeField] private AudioMixer mixer;
+
+    [Header("Events")]
+    public UnityEvent onMute;
+    public UnityEvent onUnmute;
+
+    private bool isMute;
+
+    public void ToggleMute()
+    {
+      if (isMute)
+        Unmute();
+      else
+        Mute();
+    }
+
+    public void Mute()
+    {
+      mixer.SetFloat("Master", 0);
+    }
+
+    public void Unmute()
+    {
+      mixer.SetFloat("Master", 1);
+    }
+  }
+}
