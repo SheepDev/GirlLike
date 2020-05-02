70ba5b24387b5b9b3e13e9d0a6e572c00c56af41
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-05-01 21:17:52 -0300

    Create CharacterSelect.cs, InteractivePlayer.cs

---
+++ Assets/GirlLike/Script/Runtime/Player/CharacterSelect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Orb.GirlLike.Players
+{
+  public class CharacterSelect : InteractivePlayer
+  {
+    private Animator animator;
+    [SerializeField] private bool isAvaliable;
+
+    private void Awake()
+    {
+      animator = GetComponentInChildren<Animator>();
+      Avaliable(isAvaliable);
+    }
+
+    public override void Interactive(Player player)
+    {
+      if (!isAvaliable) return;
+      Avaliable(false);
+    }
+
+    public void Avaliable(bool isAvaliable)
+    {
+      this.isAvaliable = isAvaliable;
+      animator.Play(isAvaliable ? "Loop" : "Empty");
+    }
+  }
+}
---
+++ Assets/GirlLike/Script/Runtime/Player/InteractivePlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Orb.GirlLike.Players
+{
+  public class InteractivePlayer : MonoBehaviour
+  {
+    public virtual void Interactive(Player player)
+    {
+      Debug.Log("Player");
+    }
+  }
+}
