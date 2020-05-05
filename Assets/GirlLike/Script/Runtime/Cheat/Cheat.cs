577803738a91297ab8de25a9e14908cd8bed6e73
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-05-04 21:27:01 -0300

    Add invunerable cheat

---
+++ Assets/GirlLike/Script/Runtime/Cheat/Cheat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Orb.GirlLike.Combats;
+using Orb.GirlLike.Players;
+using UnityEngine;
+
+namespace Orb.GirlLike.Cheat
+{
+  public class Cheat : MonoBehaviour
+  {
+    private Player player;
+    private GameObject hitpointGameObject;
+
+    private void Start()
+    {
+      player = GameMode.Current.GetPlayer();
+      hitpointGameObject = player.GetComponentInChildren<TakeDamage>(true).gameObject;
+    }
+
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.I))
+      {
+        hitpointGameObject.SetActive(!hitpointGameObject.activeInHierarchy);
+      }
+    }
+  }
+}
