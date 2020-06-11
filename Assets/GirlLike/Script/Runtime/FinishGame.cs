ddd4a7b8c1ca78e229852317104dcb753129676a
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-06-11 00:33:37 -0300

    Add boss die animation

---
+++ Assets/GirlLike/Script/Runtime/FinishGame.cs
@@ -0,0 +1,24 @@
+﻿using Orb.GirlLike.Players;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Orb.GirlLike
+{
+  public class FinishGame : MonoBehaviour
+  {
+    public PlayableDirector playableDirector;
+    private Player player;
+
+    private void Start()
+    {
+      player = GameMode.Current.GetPlayer();
+    }
+
+    public void Finish()
+    {
+      player.HiddenHUD(true);
+      Time.timeScale = 0;
+      playableDirector.Play();
+    }
+  }
+}
---
+++ Assets/GirlLike/Script/Runtime/MaterialProperty.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Orb.GirlLike
+{
+  [ExecuteInEditMode]
+  public class MaterialProperty : MonoBehaviour
+  {
+    public Material material;
+    public float value;
+    public Color color;
+
+    private void Update()
+    {
+      material.SetFloat("_Range", value);
+      material.SetColor("_ColorMask", color);
+    }
+  }
+}
