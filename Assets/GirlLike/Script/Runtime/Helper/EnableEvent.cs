12fabca2cf99f7bf8ee1b5b2f75cb238ec25831d
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-06-11 02:17:59 -0300

    Add player sfx

---
+++ Assets/GirlLike/Script/Runtime/Helper/EnableEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Orb.GirlLike.Helper
+{
+  public class EnableEvent : MonoBehaviour
+  {
+    public UnityEvent onEnable;
+
+    private void OnEnable()
+    {
+      onEnable.Invoke();
+    }
+  }
+}
