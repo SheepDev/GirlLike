4070cd5aee859ab68547dfcfb2e0ac458ef86bad
Francisco Lucas <franciscolucascostaalves@gmail.com>
2020-06-11 21:05:42 -0300

    Add minimap

---
+++ Assets/GirlLike/Script/Runtime/Minimap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Orb.GirlLike.Helper
+{
+  public class Minimap : MonoBehaviour
+  {
+    public Transform icon;
+    public Transform[] positions;
+    public static Minimap Current;
+
+    private void Awake()
+    {
+      Current = this;
+    }
+
+    public void SetPosition(int index)
+    {
+      icon.position = positions[index].position;
+    }
+  }
+}
